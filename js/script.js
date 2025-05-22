d3a0e7fb1e391d8ced38815338778eb36d57dfa4
Lyndon Panton <LyndonPanton@gmail.com>
2025-05-22 22:01:22 +0100

    Feature: Add calculator number interface elements

--- js/script.js
+++ js/script.js
@@ -1,12 +1,9 @@
+
+
 document.addEventListener("DOMContentLoaded", function() {
     let operand1;
     let operator;
     let operand2;
 
     let calculator = new Calculator();
-    
-    console.log(calculator.operate(10, "+", 20));
-    console.log(calculator.operate(10, "-", 20));
-    console.log(calculator.operate(10, "*", 20));
-    console.log(calculator.operate(10, "/", 20));
 });
