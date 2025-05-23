7e49c62c64dd53686c4717c0178c2a0992883837
Lyndon Panton <LyndonPanton@gmail.com>
2025-05-23 19:14:49 +0100

    Package: Installed jest

--- js/calculator.spec.js
+++ js/calculator.spec.js
@@ -1,1 +1,120 @@
-const Calcultor = require("./calculator");
+const calculator = require("./calculator");
+
+/*
+    - Test 1: small positive and small positive
+    - Test 2: large positive and small positive
+    - Test 3: large positive and large positive
+    - Test 4: small negative and small negative
+    - Test 5: large negative and large negative
+    - Test 6: positive and negative
+*/
+
+describe("addition", function () {
+    test.skip("", function () {
+        expect().toEqual();
+    });
+    
+    test.skip("", function () {
+        expect().toEqual();
+    });
+
+    test.skip("", function () {
+        expect().toEqual();
+    });
+
+    test.skip("", function () {
+        expect().toEqual();
+    });
+
+    test.skip("", function () {
+        expect().toEqual();
+    });
+});
+
+describe("subtraction", function () {
+    test.skip("", function () {
+        expect().toEqual();
+    });
+    
+    test.skip("", function () {
+        expect().toEqual();
+    });
+
+    test.skip("", function () {
+        expect().toEqual();
+    });
+
+    test.skip("", function () {
+        expect().toEqual();
+    });
+
+    test.skip("", function () {
+        expect().toEqual();
+    });
+});
+
+describe("multiplication", function () {
+    test.skip("", function () {
+        expect().toEqual();
+    });
+    
+    test.skip("", function () {
+        expect().toEqual();
+    });
+
+    test.skip("", function () {
+        expect().toEqual();
+    });
+
+    test.skip("", function () {
+        expect().toEqual();
+    });
+
+    test.skip("", function () {
+        expect().toEqual();
+    });
+});
+
+describe("divison", function () {
+    test.skip("", function () {
+        expect().toEqual();
+    });
+    
+    test.skip("", function () {
+        expect().toEqual();
+    });
+
+    test.skip("", function () {
+        expect().toEqual();
+    });
+
+    test.skip("", function () {
+        expect().toEqual();
+    });
+
+    test.skip("", function () {
+        expect().toEqual();
+    });
+});
+
+describe("remainder", function () {
+    test.skip("", function () {
+        expect().toEqual();
+    });
+    
+    test.skip("", function () {
+        expect().toEqual();
+    });
+
+    test.skip("", function () {
+        expect().toEqual();
+    });
+
+    test.skip("", function () {
+        expect().toEqual();
+    });
+
+    test.skip("", function () {
+        expect().toEqual();
+    });
+});
